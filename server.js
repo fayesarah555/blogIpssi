f28ac0b60f0e47b52502e4d382c99e3f7be11f2d
Nah0502 <160116569+Nah0502@users.noreply.github.com>
2024-03-14 17:12:49 +0100

    test

---
+++ server.js
@@ -0,0 +1,42 @@
+const cors = require("cors"),
+    express = require('express'),
+    dotenv  = require('dotenv'),
+    path    = require('path');
+dotenv.config({ path: path.resolve(__dirname, '.env')})
+require('./app/models/user')
+const sequelize = require("./app/config/db");
+
+const app = express()
+app.use(cors({
+    origin: '*',
+    method: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT']
+}))
+
+// encodade de l'url
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+require('./app/routes')(app)
+
+app.get('/', (req, res) => {
+    return res.status(200).send('Hello world')
+})
+
+app.use((req, res) => {
+    return res.status(404).send('Not found')
+})
+
+app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, async (err) => {
+    if(err){
+        console.log('Error in server setup')
+    }
+    else {
+        console.log(`Server running at http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`)
+        // update table if exist without delete
+        //await sequelize.sync({ alter: true });
+        // drop and create table
+        //await sequelize.sync({ force: true });
+        // create table if not exist
+       // await sequelize.sync();
+    }
+})
